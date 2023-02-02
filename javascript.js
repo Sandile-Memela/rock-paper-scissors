98dbb1bef140719248c3fd86929ec8c7b6216192
Sandile-Memela <sah.memela@gmail.com>
2023-02-02 17:58:45 +0200

    Added playRound function

--- javascript.js
+++ javascript.js
@@ -10,6 +10,25 @@ function getComputerChoice() {
     }
 }
 
+// playerSelection and computerSelection, return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+function playRound(playerSelection, computerSelection) {
+    let cleanPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let cleanComputerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1).toLowerCase();
+    if ((cleanComputerSelection === 'Rock' && cleanPlayerSelection === 'Scissors') || 
+        (cleanComputerSelection === 'Scissors' && cleanPlayerSelection === 'Paper') ||
+        (cleanComputerSelection === 'Paper' && cleanPlayerSelection === 'Rock')) {
+        return `You Lose! ${cleanComputerSelection} beats ${cleanPlayerSelection}.`;
+    } else if ((cleanPlayerSelection === 'Rock' && cleanComputerSelection === 'Scissors') || 
+                (cleanPlayerSelection === 'Scissors' && cleanComputerSelection === 'Paper') ||
+                (cleanPlayerSelection === 'Paper' && cleanComputerSelection === 'Rock')) {
+        return `You Win! ${cleanPlayerSelection} beats ${cleanComputerSelection}.`;
+    } else {
+        return `Tie! ${cleanPlayerSelection} ties ${cleanComputerSelection}.`;
+    }
+}
+
 for (let i = 1; i <= 30; i++) {
-    console.log(i, getComputerChoice());
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    console.log(i, playRound(playerSelection, computerSelection));
 }
