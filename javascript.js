045813ccfbb55073332ab0d1bacaf9e5e50ce238
Sandile-Memela <sah.memela@gmail.com>
2023-02-02 17:28:13 +0200

    Add getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0) {
+        return 'Rock';
+    } else if (choice === 1) {
+        return 'Paper';
+    } else if (choice === 2) {
+        return 'Scissors';
+    }
+}
+
+for (let i = 1; i <= 30; i++) {
+    console.log(i, getComputerChoice());
+}
